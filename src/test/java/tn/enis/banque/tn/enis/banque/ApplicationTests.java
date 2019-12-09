770d1bfc8515e9d44c113f235b7b5c65f05f0489
PC <lehyania@gmail.com>
2019-12-09 18:24:08 +0100

    dd

---
+++ src/test/java/tn/enis/banque/tn/enis/banque/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enis.banque.tn.enis.banque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
